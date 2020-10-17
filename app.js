24c85c8ee87eeb1b1c9462a494a0db87b8e86fce
JanuaryAle <aleksatokarieva@gmail.com>
2020-10-17 21:41:43 +0300

    added env

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express')
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/auth')
 const friendRoutes = require('./routes/friend')
-const keys = require('./config/keys')
+
 const app = express()
 
 
-mongoose.connect(keys.mongoURI, {
+mongoose.connect(process.env.mongoURI, {
     useNewUrlParser: true,
 })
 .then(() => console.log("MongoDB connected"))
 .catch(error => console.log(error))
 
 app.use(require('morgan')('dev'))
-app.use(require('cors')())
+// app.use(require('cors')())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use('/api/auth', authRoutes)
--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: `mongodb+srv://maksim1:2b1WYfRLfyh70UwU@botdatabase.xls31.mongodb.net/BBDB?retryWrites=true&w=majority`
-}
